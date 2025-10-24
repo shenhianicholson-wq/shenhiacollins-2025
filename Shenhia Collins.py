fe0cfe334b3143d66f5cd2e9839718ed90017f7c
Shenhia Nicholson-Collins <shenhianicholson@gmail.com>
2025-10-23 22:28:16 -0400

    “Added sample file for tutorial”

---
+++ Shenhia Collins.py
@@ -0,0 +1,5 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+============= RESTART: C:/Users/Dwane/OneDrive/Documents/Shenhia.py ============
+Hello, my name is Shenhia Nicholson-Collins!
